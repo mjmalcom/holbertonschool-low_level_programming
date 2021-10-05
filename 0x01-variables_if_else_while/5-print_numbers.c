f7f4af71de3d47d183c5564c9cfbaf317c29bf9c
mjmalcom <3649@holbertonschool.com>
2021-10-05 13:07:10 -0700

    Task 4

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ *main - 
+ *
+ *prints all single digit numbers of base 10 starting from 0, followed by a new line.
+ */
+int main(void)
+{
+	char i;
+
+	for (i = '0'; i <= '9'; i++)
+	{
+		putchar(i);
+	}
+	putchar('\n');
+	return (0);
+}
