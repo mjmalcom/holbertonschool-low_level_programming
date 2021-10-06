53740f4b43c309b4f52a4ba1bf220534ffda3d26
mjmalcom <3649@holbertonschool.com>
2021-10-06 08:30:38 -0700

    task 6

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * main - single digit numbers of base 10
+ *
+ * Return: 0
+ */
+int main(void)
+{
+	int i;
+
+	for (i = '0'; i <= '9'; i++)
+	{
+		putchar(i);
+	}
+	putchar('\n');
+	return (0);
+}
